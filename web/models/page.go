e60e1d268df7024a20e6ab0c837c7e6339af1f44
P-arsentiy <poltavchenko.artiy@gmail.com>
2022-10-07 19:30:56 +0300

    new items

--- web/models/page.go
+++ web/models/page.go
@@ -1,26 +1,24 @@
 package moduls
 
-
 type NavBarItemLink struct {
 	Name string
 	Link string
 }
 
 type NavBarItem struct {
-	Name string
+	Name  string
 	Links []NavBarItemLink
 }
 
-
 type TraidingToolsItem struct {
-	Icon string
-	Title string
-	Description string 
-	NameItem string
+	Icon        string
+	Title       string
+	Description string
+	NameItem    string
 }
 
 type HomePage struct {
-	NavBar []NavBarItem
+	NavBar        []NavBarItem
 	TraidingTools []TraidingToolsItem
 	PricingPlans
 	AllTools
