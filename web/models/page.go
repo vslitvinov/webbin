0932d4050dad7163144b5e9535da4f746e8f232a
vslitvinov <110770323+vslitvinov@users.noreply.github.com>
2022-10-07 21:07:52 +0500

    page

--- web/models/page.go
+++ web/models/page.go
@@ -1,1 +1,28 @@
 package moduls
+
+
+type NavBarItemLink struct {
+	Name string
+	Link string
+}
+
+type NavBarItem struct {
+	Name string
+	Links []NavBarItemLink
+}
+
+
+type TraidingToolsItem struct {
+	Icon string
+	Title string
+	Description string 
+	NameItem string
+}
+
+type HomePage struct {
+	NavBar []NavBarItem
+	TraidingTools []TraidingToolsItem
+	PricingPlans
+	AllTools
+	FAQ
+}
