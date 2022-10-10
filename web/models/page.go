2b0252e6b4b4a76e6699dd0eac8a36f83477f44e
vslitvinov <110770323+vslitvinov@users.noreply.github.com>
2022-10-10 23:42:29 +0500

    up

--- web/models/page.go
+++ web/models/page.go
@@ -14,13 +14,24 @@ type TraidingToolsItem struct {
 	Icon        string
 	Title       string
 	Description string
-	NameItem    string
+	Link    string
 }
 
 type HomePage struct {
 	ServiseBlock ServiseBlock
+	AllTools []Tool
+	FAQ Faqs
 }
 
+type Faqs struct {
+	Question string
+	Answer string 
+}
+
+type Tool struct {
+	Title string 
+	Text string
+}
 
 type ServiseBlock struct {
 	TraidingTools []TraidingToolsItem
