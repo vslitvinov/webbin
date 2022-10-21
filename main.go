5f43c9cace8279f2b2cd11bc122cb20ba6c93522
vslitvinov <110770323+vslitvinov@users.noreply.github.com>
2022-10-21 20:59:32 +0500

    Update main.go

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func main() {
 
 
 	fmt.Printf("Server listening on %s", server.Addr)
-	if err := server.ListenAndServeTLS("output.crt", "key.key"); err != nil {
+	if err := server.ListenAndServeTLS("output.crt", "key.pem"); err != nil {
 		fmt.Println(err)
 	}
 
