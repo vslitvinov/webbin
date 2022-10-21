3871d6fce4b07485b2411f4ff6a697ef71d348eb
vslitvinov <110770323+vslitvinov@users.noreply.github.com>
2022-10-21 17:17:58 +0500

    https

--- main.go
+++ main.go
@@ -6,15 +6,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"path/filepath"
 	"site/webserver"
 	"site/webserver/modules"
-
-	"golang.org/x/crypto/acme/autocert"
 )
 
 var host = "0.0.0.0"
 var port = "443"
+var domain = "eterinte.com"
 
 func main() {
 
@@ -42,48 +40,30 @@ func main() {
 	defer db.Close()
 
 	site := webserver.NewWebServer(db)
-	domain := "eterinte.com"
-	// addr := fmt.Sprintf("%s:%s", host, port)
-	certManager := autocert.Manager{
-		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist(domain),
-		Cache:      autocert.DirCache("certs"),
-	}
 
-	tlsConfig := certManager.TLSConfig()
-	tlsConfig.GetCertificate = getSelfSignedOrLetsEncryptCert(&certManager)
 	server := http.Server{
-		Addr:      ":443",
-		Handler:   site.Router,
-		TLSConfig: tlsConfig,
+		Addr:    ":443",
+		Handler: site.Router,
+		TLSConfig: &tls.Config{
+			NextProtos: []string{"h2", "http/1.1"},
+		},
 	}
-	err = server.ListenAndServe()
-	// log.Fatal(srv.ListenAndServeTLS("./server.crt", "./server.key"))
-	// err = http.Serve(autocert.NewListener(domain), site.Router)
-	// err = http.ListenAndServeTLS(addr, "./server.crt", "./server.key", site.Router)
 
-	if err != nil {
-		log.Fatal(fmt.Sprintf("Не удалось запустить сервер: %v", err))
+	go func() {
+		if err := http.ListenAndServe(":80", http.HandlerFunc(redirectTLS)); err != nil {
+			log.Fatalf("ListenAndServe error: %v", err)
+		}
+	}()
+
+
+	fmt.Printf("Server listening on %s", server.Addr)
+	if err := server.ListenAndServeTLS("server.crt", "server.key"); err != nil {
+		fmt.Println(err)
 	}
 
 }
 
 
-func getSelfSignedOrLetsEncryptCert(certManager *autocert.Manager) func(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
-  return func(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
-    dirCache, ok := certManager.Cache.(autocert.DirCache)
-    if !ok {
-      dirCache = "certs"
-    }
-
-    keyFile := filepath.Join(string(dirCache), hello.ServerName+".key")
-    crtFile := filepath.Join(string(dirCache), hello.ServerName+".crt")
-    certificate, err := tls.LoadX509KeyPair(crtFile, keyFile)
-    if err != nil {
-      fmt.Printf("%s\nFalling back to Letsencrypt\n", err)
-      return certManager.GetCertificate(hello)
-    }
-    fmt.Println("Loaded selfsigned certificate.")
-    return &certificate, err
-  }
+func redirectTLS(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://"+domain+":443"+r.RequestURI, http.StatusMovedPermanently)
 }
