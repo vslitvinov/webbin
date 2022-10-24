ecc56b1cefa5a29a72304f300c312f6696cc1f32
P-arsentiy <poltavchenko.artiy@gmail.com>
2022-10-24 16:45:37 +0300

    cert up

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func main() {
 	// }()
 
 	fmt.Printf("Server listening on %s", server.Addr)
-	if err := server.ListenAndServeTLS("eterinte_com.crt", "key.pem"); err != nil {
+	if err := server.ListenAndServeTLS("eterinte_com.crt", "eterinte_com.ca-bundle"); err != nil {
 		fmt.Println(err)
 	}
 
