8c0ce232b3222400b915d12470d4fd324e725e24
P-arsentiy <poltavchenko.artiy@gmail.com>
2022-10-26 12:55:52 +0300

    navbar-stiky changes

--- webserver/assets/js/app.js
+++ webserver/assets/js/app.js
@@ -123,8 +123,8 @@ function windowScroll() {
     const navbar = document.getElementById("topnav");
     if(navbar!=null){
         if (
-            document.body.scrollTop >= 50 ||
-            document.documentElement.scrollTop >= 50
+            document.body.scrollTop >= 0 ||
+            document.documentElement.scrollTop >= 0
         ) {
             navbar.classList.add("nav-sticky");
         } else {
